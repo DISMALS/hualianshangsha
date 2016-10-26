72f95be7727bff80d59a4693bd8eec0b79b03225
Dismal <1139730843@qq.com>
2016-10-26 17:49:26 +0800

    hualianshangsha
    郑州华联商厦

---
+++ Gruntfile.js
@@ -0,0 +1,93 @@
+﻿"use strict";
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        uglify: {
+            src: ["pc/js/base.js"],
+            dest: "pc/js/base.min.js"
+        },
+        cssmin: {
+            one: {
+                options: {
+                    banner: "/*制作时间：2016.6.13\n作者：王勇\n用途：郑州华联商厦pc网站首页样式\n*/"
+                },
+                src: ["pc/css/index.css"],
+                dest: "pc/css/index.min.css"
+            },
+            two: {
+                options: {
+                    banner: "/*制作时间：2016.6.13\n作者：王勇\n用途：郑州华联商厦pc网站其他页面样式\n*/"
+                },
+                src: ["pc/css/other.css"],
+                dest: "pc/css/other.min.css"
+            },
+            three: {
+                options: {
+                    banner: "/*制作时间：2016.6.13\n作者：王勇\n用途：郑州华联商厦pc网站其他页面样式\n*/"
+                },
+                src: ["pc/css/list.css"],
+                dest: "pc/css/list.min.css"
+            },
+            four: {
+                options: {
+                    banner: "/*制作时间：2016.6.13\n作者：王勇\n用途：郑州华联商厦pc网站首页样式\n*/"
+                },
+                src: ["mobile/css/index.css"],
+                dest: "mobile/css/index.min.css"
+            },
+            five: {
+                options: {
+                    banner: "/*制作时间：2016.6.13\n作者：王勇\n用途：郑州华联商厦pc网站其他页面样式\n*/"
+                },
+                src: ["mobile/css/other.css"],
+                dest: "mobile/css/other.min.css"
+            },
+            six: {
+                options: {
+                    banner: "/*制作时间：2016.6.13\n作者：王勇\n用途：郑州华联商厦pc网站其他页面样式\n*/"
+                },
+                src: ["mobile/css/list.css"],
+                dest: "mobile/css/list.min.css"
+            }
+            
+        },
+        watch: {
+            one:{
+                files: ["pc/css/index.css"],
+                tasks: ["cssmin:one"]
+            },
+            two:{
+                files: ["pc/css/other.css"],
+                tasks: ["cssmin:two"]
+            },
+            three: {
+                files: ["pc/css/list.css"],
+                tasks: ["cssmin:three"]
+            },
+            four: {
+                files: ["mobile/css/index.css"],
+                tasks: ["cssmin:four"]
+            },
+            five: {
+                files: ["mobile/css/other.css"],
+                tasks: ["cssmin:five"]
+            },
+            six: {
+                files: ["mobile/css/list.css"],
+                tasks: ["cssmin:six"]
+            }
+        }
+    });
+    //加载任务插件
+    grunt.loadNpmTasks("grunt-contrib-cssmin");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+
+    //执行任务
+    grunt.registerTask("one", "这是压缩的css文件的任务", ["watch:one"]);
+    grunt.registerTask("two", "这是压缩的css文件的任务", ["watch:two"]);
+    grunt.registerTask("three", "这是压缩css文件的任务", ["watch:three"]);
+    grunt.registerTask("four", "这是压缩的css文件的任务", ["watch:four"]);
+    grunt.registerTask("five", "这是压缩的css文件的任务", ["watch:five"]);
+    grunt.registerTask("six", "这是压缩css文件的任务", ["watch:six"]);
+}
---
+++ mobile/js/base.js
@@ -0,0 +1,100 @@
+﻿$(document).ready(function () {
+    //选项卡
+    function tabS(clickBtn, showMain, siblingsC) {
+        var abstractMenuA = $(clickBtn);
+        var MTextOne = $(showMain);
+        abstractMenuA.bind("click", function () {
+            var this_ = $(this);
+            if (this_.hasClass("on")) {
+                MTextOne.eq(abstractMenuA.index(this)).show().siblings().hide();
+            } else {
+                this_.addClass("on").siblings(siblingsC).removeClass("on");
+                MTextOne.eq(abstractMenuA.index(this)).show().siblings().hide();
+            }
+        });
+    }
+    tabS("ul.slideTit li", "div.slideList", "li");
+    tabS("ul.slidesTit li", "ul.slidesList", "li");
+
+    //下拉导航菜单
+    $(".menu").bind("click", function () {
+        var this_ = $(this);
+        if (this_.hasClass("zk")) {
+            $(".menuList").slideUp(100);
+            this_.removeClass("zk");
+        } else {
+            this_.addClass("zk");
+            $(".menuList").slideDown(100, function () {
+                var this_s = $(this);
+                this_s.find("li").click(function () {
+                    this_s.slideUp(100);
+                });
+            });
+        }
+    });
+
+    //轮播图
+    TouchSlide({
+        slideCell: "#slider",
+        titCell: ".sliderBtn ul", //开启自动分页 autoPage:true ，此时设置 titCell 为导航元素包裹层
+        mainCell: ".sliderM ul",
+        effect: "left",
+        autoPlay: true,//自动播放
+        autoPage: true, //自动分页
+        switchLoad: "_src" //切换加载，真实图片路径为"_src"
+    });
+
+    //英才招聘模拟下拉
+    $(".xiala").bind("click", function () {
+        var this_ = $(this);
+        var mainMH = this_.siblings(".mainM").height();
+        var zjd = this_.parents(".module").siblings();
+        var tjA = zjd.find(".xiala");
+        if (tjA.hasClass("cli")) {
+            this_.css("display", "none").addClass("cli");
+            this_.parent(".contentMain").animate({
+                height: mainMH + "px"
+            }, 150);
+            this_.siblings(".mainM").find(".onlineZp").css("visibility", "visible");
+            tjA.css("display", "block").removeClass("cli");
+            tjA.parent(".contentMain").animate({
+                height: "5.15rem"
+            }, 150);
+            this_.parents(".module").siblings().find(".onlineZp").css("visibility", "hidden");
+        } else {
+            this_.css("display", "none").addClass("cli");
+            this_.parent(".contentMain").animate({
+                height: mainMH + "px"
+            }, 150);
+            this_.siblings(".mainM").find(".onlineZp").css("visibility", "visible");
+        }
+    });
+
+    //大事记下拉
+    $(".eventsTit").bind("click", function () {
+        var this_ = $(this);
+        if (this_.hasClass("eventsNor")) {
+            this_.removeClass("eventsNor").addClass("eventsCli");
+            this_.siblings().slideDown(150);
+        } else {
+            this_.removeClass("eventsCli").addClass("eventsNor");
+            this_.siblings().slideUp(150);
+        }
+    });
+
+    //分布平面图轮播
+    TouchSlide({
+        slideCell: "#areaPicM",
+        mainCell: ".areaPicSlid ul",
+        effect: "leftLoop",
+        titCell: ".areaPicBtn ul", //开启自动分页 autoPage:true ，此时设置 titCell 为导航元素包裹层
+        autoPlay: true,//自动播放
+        autoPage: true //自动分页
+    });
+
+    //返回顶部
+    $(".backTop").click(function () {
+        $(window).scrollTop(0);
+    });
+    
+});
